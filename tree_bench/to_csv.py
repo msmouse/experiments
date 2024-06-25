f5dcf5321b48dc22a4630e839a53ce1ba04ec813
aldenhu <msmouse@gmail.com>
2024-06-25 19:03:11 +0000

    data

---
+++ tree_bench/to_csv.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+# note: before going through this, filter log with:
+#     `grep -P 'name: |thrpt:.*Kelem'`
+
+import csv
+import sys
+import json
+import re
+
+
+FIELD_NAMES = [
+    'name',
+    'set_size_m',
+    'batch_size_k',
+    'arity',
+    'total_levels',
+    'total_memory_m',
+    'overlapping_top_levels',
+    'sparse_levels',
+    'total_hashing',
+    'disk_bytes_per_update',
+    'k_updates_per_sec',
+]
+writer = csv.DictWriter(sys.stdout, fieldnames=FIELD_NAMES)
+writer.writeheader()
+
+
+record = None
+for line in sys.stdin:
+    if record is None:
+        line = re.sub(r"(\w+):", '"\\1":', line)
+        line = line.replace("'", '"')
+        record = json.loads(line)
+    else:
+        m = re.search(r"thrpt:\s+\[([\d.]+) Kelem", line)
+        if not m:
+            raise "Didn't match"
+        record['k_updates_per_sec'] = float(m.group(1))
+        writer.writerow(record)
+        record = None
