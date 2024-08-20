a2fda73b2b44fa93f5347dce589ebeb1f605a612
aldenhu <msmouse@gmail.com>
2024-08-20 01:22:08 +0000

    bench hexy

--- tree_bench/benches/hashing.rs
+++ tree_bench/benches/hashing.rs
@@ -1,8 +1,13 @@
 use aptos_crypto::HashValue;
+use aptos_experimental_hexy::in_mem::base::HexyBase;
+use aptos_experimental_hexy::in_mem::overlay::HexyOverlay;
+use aptos_experimental_hexy::LeafIdx;
 use criterion::measurement::WallTime;
-use criterion::{criterion_group, criterion_main, BenchmarkGroup, Criterion};
+use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkGroup, Criterion};
 use fastcrypto::hash::{EllipticCurveMultisetHash, MultisetHash};
+use rand::Rng;
 use rayon::prelude::*;
+use std::sync::Arc;
 
 fn inc_hash(c: &mut Criterion) {
     let mut group = c.benchmark_group("inc_hashing");
@@ -97,7 +102,11 @@ fn n_parent_nodes(arity: usize, n_nodes: usize) -> usize {
     n_nodes / arity + (n_nodes % arity != 0) as usize
 }
 
-fn num_complete_tree_internal_nodes_to_update(arity: usize, n_total_leaves: usize, n_updated_leaves: usize) -> usize {
+fn num_complete_tree_internal_nodes_to_update(
+    arity: usize,
+    n_total_leaves: usize,
+    n_updated_leaves: usize,
+) -> usize {
     let mut n_level_total = n_total_leaves;
     let mut n_level_updated = n_updated_leaves;
     let mut n_total_updated = 0;
@@ -114,7 +123,7 @@ fn num_complete_tree_internal_nodes_to_update(arity: usize, n_total_leaves: usiz
         n_level_total = parent_level_total;
     }
 
-    return n_total_updated
+    return n_total_updated;
 }
 
 fn complete_merkle_tree_sim(
@@ -127,13 +136,18 @@ fn complete_merkle_tree_sim(
     const K: usize = 1024;
     const HASH_SIZE: usize = 32;
 
-    let name = format!("arity_{}_leaves_{}m_batch_{}k", arity, set_size_m, batch_size_k);
+    let name = format!(
+        "arity_{}_leaves_{}m_batch_{}k",
+        arity, set_size_m, batch_size_k
+    );
     println!("-- {name}: calculating parameters");
 
-    let total_nodes = num_complete_tree_internal_nodes_to_update(arity, set_size_m * M, set_size_m * M);
+    let total_nodes =
+        num_complete_tree_internal_nodes_to_update(arity, set_size_m * M, set_size_m * M);
     let total_memory_m = total_nodes * HASH_SIZE / M;
 
-    let num_hashing_per_batch = num_complete_tree_internal_nodes_to_update(arity, set_size_m * M, batch_size_k * K);
+    let num_hashing_per_batch =
+        num_complete_tree_internal_nodes_to_update(arity, set_size_m * M, batch_size_k * K);
     let disk_bytes_per_update = num_hashing_per_batch * HASH_SIZE / batch_size_k / K;
 
     println!("(not counting leaves):");
@@ -181,10 +195,77 @@ fn complete_merkle_tree_sims(c: &mut Criterion) {
     }
 }
 
+fn gen_hexy_updates(batch_size_k: usize, set_size_m: usize) -> Vec<(LeafIdx, HashValue)> {
+    const M: usize = 1024 * 1024;
+    const K: usize = 1024;
+
+    (0..batch_size_k * K)
+        .map(|_| {
+            (
+                rand::thread_rng().gen_range(0..(set_size_m * M) as u32),
+                HashValue::random(),
+            )
+        })
+        .collect()
+}
+
+fn hexy_update(
+    group: &mut BenchmarkGroup<WallTime>,
+    batch_size_k: usize,
+    set_size_m: usize,
+    pipeline_depth: usize,
+) {
+    const M: usize = 1024 * 1024;
+
+    println!("Allocating base: {set_size_m}M items");
+    let base = Arc::new(HexyBase::allocate((set_size_m * M) as u32));
+    let root_overlay = HexyOverlay::new_empty(&base);
+    let mut base_overlay = root_overlay.clone();
+    println!("Prepare pipeline of depth {pipeline_depth}");
+    for _ in 0..pipeline_depth {
+        let updates = gen_hexy_updates(batch_size_k, set_size_m);
+        base_overlay = base_overlay
+            .view(&base, &root_overlay)
+            .new_overlay(updates)
+            .unwrap();
+    }
+    let updates = gen_hexy_updates(batch_size_k, set_size_m);
+
+    group.throughput(criterion::Throughput::Elements(batch_size_k as u64 * 1024));
+    let name = format!(
+        "hexy_update_leaves_{}m_batch_{}k_pipeline_depth_{}",
+        set_size_m, batch_size_k, pipeline_depth
+    );
+    group.bench_function(&name, |b| {
+        b.iter_batched(
+            || updates.clone(),
+            |updates| {
+                base_overlay
+                    .view(&base, &root_overlay)
+                    .new_overlay(updates)
+                    .unwrap()
+            },
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+fn hexy_updates(c: &mut Criterion) {
+    let mut group = c.benchmark_group(format!("hexy_updates"));
+
+    for set_size_m in [32, 64, 128] {
+        for batch_size_k in [1, 10] {
+            for pipeline_depth in [0, 2, 8] {
+                hexy_update(&mut group, batch_size_k, set_size_m, pipeline_depth);
+            }
+        }
+    }
+}
+
 criterion_group!(
     name = hashing;
     config = Criterion::default();
-    targets = inc_hash, inc_hash_parallel, complete_merkle_tree_sims,
+    targets = inc_hash, inc_hash_parallel, hexy_updates, complete_merkle_tree_sims
 );
 
 criterion_main!(hashing);
